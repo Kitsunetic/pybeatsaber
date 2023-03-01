61db12f60fbd74af8ebf82b4c3d39051f36af470
kitsunetic <jh.shim.gg@gmail.com>
2023-03-01 11:27:22 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pybeatsaber",
+    version="0.0.1",
+    description="BeatSaber Beatmap IO Library written in pure python",
+    author="Kitsunetic",
+    author_email="jh.shim.gg@gmail.com",
+    url="https://github.com/Kitsunetic/pybeatsaber.git",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={},
+    setup_requires=[],
+    tests_require=[],
+)
